c1c439738f5c2286c8a63bde87271834df2f6ff3
Mazin Assanie <mazina@umich.edu>
2017-06-16 14:11:15 -0400

    Launch scripts will now copy appropriate SWT for platform on launch

--- scripts/linux/SoarJavaDebugger.sh
+++ scripts/linux/SoarJavaDebugger.sh
@@ -1,4 +1,21 @@
-#!/bin/bash
-export SOAR_HOME="$(pwd)/bin"
-export LD_LIBRARY_PATH="$SOAR_HOME"
-java -Djava.library.path="$SOAR_HOME" -jar "$SOAR_HOME/SoarJavaDebugger.jar" $1 $2 $3 $4 $5 &
+#!/usr/bin/env bash
+
+THISDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+FLAG=""
+export SOAR_HOME="$THISDIR/bin"
+export DYLD_LIBRARY_PATH="$SOAR_HOME"
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    cp $SOAR_HOME/java/swt-linux64.jar $SOAR_HOME/java/swt.jar
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    cp $SOAR_HOME/java/swt-mac64.jar $SOAR_HOME/java/swt.jar
+    FLAG="-XstartOnFirstThread"
+else
+    echo 'Unsupported OS'
+    exit 1
+fi
+
+pushd $THISDIR > /dev/null
+java $FLAG -Djava.library.path="$SOAR_HOME" -jar "$SOAR_HOME/SoarJavaDebugger.jar" $1 $2 $3 $4 $5 &
+popd > /dev/null
--- scripts/linux/Tanksoar.sh
+++ scripts/linux/Tanksoar.sh
@@ -1,5 +1,21 @@
-#!/bin/bash
-export SOAR_HOME="$(pwd)/bin"
-export LD_LIBRARY_PATH="$SOAR_HOME"
-java -Djava.library.path="$SOAR_HOME" -jar "$SOAR_HOME/Eaters_TankSoar.jar" config/tanksoar.cnf
+#!/usr/bin/env bash
 
+THISDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+FLAG=""
+export SOAR_HOME="$THISDIR/bin"
+export DYLD_LIBRARY_PATH="$SOAR_HOME"
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    cp $SOAR_HOME/java/swt-linux64.jar $SOAR_HOME/java/swt.jar
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    cp $SOAR_HOME/java/swt-mac64.jar $SOAR_HOME/java/swt.jar
+    FLAG="-XstartOnFirstThread"
+else
+    echo 'Unsupported OS'
+    exit 1
+fi
+
+pushd $THISDIR > /dev/null
+java $FLAG -Djava.library.path="$SOAR_HOME" -jar "$SOAR_HOME/Eaters_TankSoar.jar" config/tanksoar.cnf
+popd > /dev/null
