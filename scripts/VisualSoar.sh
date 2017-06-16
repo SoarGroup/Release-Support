19bb89a545213b3b5ce4506cb365662442366c0b
Mazin Assanie <mazina@umich.edu>
2017-06-16 14:36:39 -0400

    Fixed new script for visual soar
    - Does not like xstartonfirstthread

--- scripts/VisualSoar.sh
+++ scripts/VisualSoar.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 THISDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-FLAG=""
 export SOAR_HOME="$THISDIR/bin"
 export DYLD_LIBRARY_PATH="$SOAR_HOME"
 
@@ -10,12 +9,11 @@ if [[ "$unamestr" == 'Linux' ]]; then
     cp $SOAR_HOME/java/swt-linux64.jar $SOAR_HOME/java/swt.jar
 elif [[ "$unamestr" == 'Darwin' ]]; then
     cp $SOAR_HOME/java/swt-mac64.jar $SOAR_HOME/java/swt.jar
-    FLAG="-XstartOnFirstThread"
 else
     echo 'Unsupported OS'
     exit 1
 fi
 
-pushd $THISDIR > /dev/null
-java $FLAG -Djava.library.path="$SOAR_HOME" -jar "$SOAR_HOME/VisualSoar.jar" $1 $2 $3 $4 $5 &
+pushd $SOAR_HOME > /dev/null
+java -Djava.library.path="$SOAR_HOME" -jar "$SOAR_HOME/VisualSoar.jar" &
 popd > /dev/null
